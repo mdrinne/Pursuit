c6435bc59717b7a9812bc278428048328da442e3
helloben <jbbailey2@crimson.ua.edu>
2020-02-24 16:50:48 -0600

    wip

--- app/src/main/java/com/example/pursuit/MainActivity.java
+++ app/src/main/java/com/example/pursuit/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String NO_USER_PASS_MESSAGE = "com.example.pursuit.NO_USER_PASS_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,20 +18,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginUser(View view) {
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+
+        EditText userText = (EditText) findViewById(R.id.username);
+        String username = userText.getText().toString();
+
+        EditText passwordText = (EditText) findViewById(R.id.password);
+        String password = passwordText.getText().toString();
 
         if (!username.isEmpty() && !password.isEmpty()) {
+            if (checkUserame() && checkPassword()) {
 
+            }
         } else {
             Intent intent = new Intent(this, RegistrationActivity.class);
+            intent.putExtra(NO_USER_PASS_MESSAGE, "You must enter both a username and password.");
             startActivity(intent);
         }
 
         Intent intent = new Intent(this, RegistrationActivity.class);
-
         startActivity(intent);
-
     }
 
     public void registerUser(View view) {
