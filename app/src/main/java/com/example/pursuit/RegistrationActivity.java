a9ca497161509f95d5935f04a2bf215341d96082
arhencey <arhencey@yahoo.com>
2020-02-24 18:54:10 -0600

    Completed the Registration activity.

--- app/src/main/java/com/example/pursuit/RegistrationActivity.java
+++ app/src/main/java/com/example/pursuit/RegistrationActivity.java
@@ -2,7 +2,13 @@ package com.example.pursuit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -11,4 +17,31 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
     }
+
+    public void continueToRegistration(View view) {
+        final RadioGroup radGroup = (RadioGroup) findViewById(R.id.studentOrCompany);
+        if (radGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please make a selection.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            int selectedID = radGroup.getCheckedRadioButtonId();
+            if (selectedID == findViewById(R.id.radioBtnStudent).getId()) {
+                Intent intent = new Intent(this, StudentRegistration.class);
+                startActivity(intent);
+            }
+            else {
+                Intent intent = new Intent(this, CompanyRegistration.class);
+                startActivity(intent);
+            }
+
+        }
+    }
+
+    //public void getSelectedRadioButton(View view) {
+    //    final RadioGroup radGroup = (RadioGroup) findViewById(R.id.studentOrCompany);
+    //    int radioID = radGroup.getCheckedRadioButtonId();
+    //    RadioButton singleButton = (RadioButton) findViewById(radioID);
+
+
+    //}
 }
