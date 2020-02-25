f9a09542020b03cb8cbbd81f06d2b117d7548d9d
Matthew Rinne <mdrinne@att.net>
2020-02-25 11:45:33 -0600

    creating student.java class for database methods

---
+++ app/src/main/java/com/example/pursuit/database/models/Student.java
@@ -0,0 +1,53 @@
+package com.example.pursuit.database.models;
+
+public class Student {
+    public static final String APPLICANT_TABLE = "Applicants";
+    public static final String APPLICANT_COL0 = "id INTEGER PRIMARY KEY";
+    public static final String APPLICANT_COL1 = "firstname VARCHAR";
+    public static final String APPLICANT_COL2 = "lastname VARCHAR";
+    public static final String APPLICANT_COL3 = "university VARCHAR";
+    public static final String APPLICANT_COL4 = "major VARCHAR";
+    public static final String APPLICANT_COL5 = "minor VARCHAR";
+    public static final String APPLICANT_COL6 = "gpa VARCHAR";
+    public static final String APPLICANT_COL7 = "bio VARCHAR";
+    public static final String APPLICANT_COL8 = "email VARCHAR";
+    public static final String APPLICANT_COL9 = "username VARCHAR";
+    public static final String APPLICANT_COL10 = "password VARCHAR";
+
+    private int id;
+    private String fname;
+    private String lname;
+    private String university;
+    private String major;
+    private String minor;
+    private String gpa;
+    private String bio;
+    private String email;
+    private String username;
+    private String password;
+
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + APPLICANT_TABLE +
+            " (" + APPLICANT_COL0 + ", " + APPLICANT_COL1 + ", " +
+            APPLICANT_COL2 + ", " + APPLICANT_COL3 + ", " + APPLICANT_COL4 + ", " +
+            APPLICANT_COL5 + ", " + APPLICANT_COL6 + ", " + APPLICANT_COL7 + ", " +
+            APPLICANT_COL8 + ", " + APPLICANT_COL9 + ", " + APPLICANT_COL10 + ");";
+
+    public Student() {
+
+    }
+
+    public Student(int id, String firstname, String lastname, String university, String major,
+                   String minor, String gpa, String bio, String email, String username, String password) {
+        this.id = id;
+        this.fname = firstname;
+        this.lname = lastname;
+        this.university = university;
+        this.major = major;
+        this.minor = minor;
+        this.gpa = gpa;
+        this.bio = bio;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
