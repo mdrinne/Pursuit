490a4562e2138b229cd2b972ab8126922ac77668
helloben <jbbailey2@crimson.ua.edu>
2020-02-25 02:08:21 -0600

    WIP. Next step is connect and insert into the db

--- app/src/main/java/com/example/pursuit/CompanyRegistration.java
+++ app/src/main/java/com/example/pursuit/CompanyRegistration.java
@@ -3,20 +3,28 @@ package com.example.pursuit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Toast;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteDatabase.OpenParams;
+
 
 public class CompanyRegistration extends AppCompatActivity {
 
     EditText companyName;
-    EditText companyPassword;
     EditText companyEmail;
+    EditText companyPassword;
     EditText companyField;
 //    Button register;
+
+    private final String DB_NAME = "pursuit.db";
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +35,43 @@ public class CompanyRegistration extends AppCompatActivity {
         companyName = findViewById(R.id.companyName);
         companyEmail = findViewById(R.id.companyEmail);
         companyField = findViewById(R.id.companyField);
+
+        try {
+            db = this.openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+
+        } catch (SQLiteException se) {
+            Log.e(getClass().getSimpleName(), "Could not create or open database");
+        }
     }
 
     public void register(View view) {
+        boolean valid = true;
         Log.d(getClass().getSimpleName(), "in checkIfEmpty");
 
         String companyNameString = companyName.getText().toString();
         Log.d(getClass().getSimpleName(), companyNameString);
 
-        String companyPasswordString = companyPassword.getText().toString();
-        Log.d(getClass().getSimpleName(), companyPasswordString);
-
         String companyEmailString = companyEmail.getText().toString();
         Log.d(getClass().getSimpleName(), companyEmailString);
+
+        String companyPasswordString = companyPassword.getText().toString();
+        Log.d(getClass().getSimpleName(), companyPasswordString);
 
         String companyFieldString = companyField.getText().toString();
         Log.d(getClass().getSimpleName(), companyFieldString);
 
+        if (companyNameString.isEmpty() || companyEmailString.isEmpty() || companyPasswordString.isEmpty()) {
+            Toast.makeText(view.getContext(), "Please fill out all required fields.", 2).show();
+            valid = false;
+        }
+
+        if (valid) {
+            Intent intent = new Intent(this, LandingActivity.class);
+            startActivity(intent);
+        }
+//        if (!companyAlreadyExists(companyEmailString, companyPasswordString)) {
+//
+//        }
     }
+
 }
