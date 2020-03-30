9e416d23ac723cf2e50639a8556d1439580c1009
arhencey <arhencey@yahoo.com>
2020-03-30 14:14:32 -0500

    Added buttonUploadPhoto

--- app/src/main/java/com/example/pursuit/StudentProfileActivity.java
+++ app/src/main/java/com/example/pursuit/StudentProfileActivity.java
@@ -2,7 +2,15 @@ package com.example.pursuit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 // import android.widget.ImageView;
 // import android.widget.LinearLayout;
@@ -19,6 +27,7 @@ public class StudentProfileActivity extends AppCompatActivity {
     TextView studentBio;
     // ImageView profilePhoto;
     Student currentStudent;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +61,36 @@ public class StudentProfileActivity extends AppCompatActivity {
         // LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width,height);
         // profilePhoto.setLayoutParams(params);
 
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView6);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 
     private void initializeCurrentStudent() {
