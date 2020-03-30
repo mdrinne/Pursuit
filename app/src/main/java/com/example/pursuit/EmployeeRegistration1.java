89ddc994e019abe964240d08ce4a30330167e798
Matthew Rinne <mdrinne@att.net>
2020-03-29 23:53:06 -0500

    created Employee class

--- app/src/main/java/com/example/pursuit/EmployeeRegistration1.java
+++ app/src/main/java/com/example/pursuit/EmployeeRegistration1.java
@@ -1,4 +1,19 @@
 package com.example.pursuit;
 
-public class EmployeeRegistration1 {
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class EmployeeRegistration1 extends AppCompatActivity {
+
+    private static final String TAG = "EmployeeRegistration1";
+
+    private DatabaseReference dbref;
+
+    EditText companyCode;
+    EditText invitationCode;
+
+
 }
