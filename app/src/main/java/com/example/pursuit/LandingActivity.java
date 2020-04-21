a4f69d948eb97828c146a517cfcee075ea28eeb7
Matthew Rinne <mdrinne@att.net>
2020-04-21 18:06:27 -0500

    removed button

--- app/src/main/java/com/example/pursuit/LandingActivity.java
+++ app/src/main/java/com/example/pursuit/LandingActivity.java
@@ -116,11 +116,4 @@ public class LandingActivity extends AppCompatActivity {
         finish();
     }
 
-    public void matchedOpportunities(View v) {
-        if (currentRole.equals("Student")) {
-            Intent intent = new Intent(LandingActivity.this, StudentMatchedOpportunities.class);
-            startActivity(intent);
-        }
-    }
-
 }
