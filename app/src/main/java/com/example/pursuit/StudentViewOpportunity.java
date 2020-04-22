f2d48c298796ffee86cd9ff06904971e819945e1
Matthew Rinne <mdrinne@att.net>
2020-04-21 21:03:36 -0500

    added student opportunity view layout

---
+++ app/src/main/java/com/example/pursuit/StudentViewOpportunity.java
@@ -0,0 +1,14 @@
+package com.example.pursuit;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class StudentViewOpportunity extends AppCompatActivity {
+    TextView companyName, position, timestamp, city, state, with, description, requirements;
+
+    BottomNavigationView bottomNavigation;
+
+}
